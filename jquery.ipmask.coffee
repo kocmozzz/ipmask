ae8c4026acd84f01d9c0cc144125ce2700a07187
Nikolay Solovyov <nikolaysolovjov@undev.ru>
2014-03-02 20:00:45 +0400

    initial commit

---
+++ jquery.ipmask.coffee
@@ -0,0 +1,92 @@
+(($) ->
+  keyCodes =
+    IGNORE: [ 65, 66, 67, 68, 69, 70, 71, 72, 73, 74, 75, 76, 77, 78, 79, 80, 81, 82, 83, 84, 85, 86, 87, 88, 89, 90, 106, 107, 109, 186, 187, 188, 189, 191, 192, 219, 220, 221, 222 ]
+    NUMBERS: [ 48, 49, 50, 51, 52, 53, 54, 55, 56, 57, 96, 97, 98, 99, 100, 101, 102, 103, 104, 105 ]
+    BACKSPACE: [ 8 ]
+    POINT: [ 190, 110 ]
+
+  codesToNumbers =
+    48: 0, 49: 1, 50: 2, 51: 3, 52:  4, 53:  5, 54:  6, 55:  7, 56:  8, 57:  9
+    96: 0, 97: 1, 98: 2, 99: 3, 100: 4, 101: 5, 102: 6, 103: 7, 104: 8, 105: 9 # numpad
+
+  go_next = no
+  go_prev = no
+
+  nextInput = (el) ->
+    next = $(el).next().next()
+    next.focus() if next.length > 0
+    return
+
+  prevInput = (el) ->
+    prev = $(el).prev().prev()
+    prev.focus() if prev.length > 0
+    return
+
+  isKey = (arr, keycode) ->
+    return arr.indexOf(keycode) isnt -1
+
+  $.fn.ip = (opts = placeholder: "...", callback: null ) ->
+    opts.placeholder = opts.placeholder?.split "."
+    @attr "type", "hidden"
+
+    # Генерируем и вставляем необходимый html
+    container = document.createElement "div"
+    container.className = "b-ipmask form-control"
+
+    html = ""
+    for i in [0..3]
+      html += "<input type='text' class='b-ipmask__input' placeholder='#{opts.placeholder[i]}' maxlength='3'>"
+      html += "<span class='b-ipmask__span'>.</span>" if i < 3
+    container.innerHTML = html
+
+    @after container
+
+    # Вешаем обработчики
+    els = $(".b-ipmask__input", container);
+
+    els.on "keydown", (e) ->
+      value = @value
+
+      if isKey keyCodes.IGNORE, e.keyCode
+        e.preventDefault()
+
+      else if isKey keyCodes.NUMBERS, e.keyCode
+        if value.length is 2
+          sum = value + codesToNumbers[e.keyCode]
+          if sum.length isnt (parseInt(sum) + "").length or sum > 255
+            e.preventDefault()
+          else
+            go_next = yes
+
+      else if isKey keyCodes.POINT, e.keyCode
+        e.preventDefault()
+        nextInput this if value isnt ""
+
+      else if isKey keyCodes.BACKSPACE, e.keyCode
+        if value.length is 0
+          prevInput this
+        else go_prev = yes if value.length is 1
+
+      return e
+
+    els.on "keyup", (e) =>
+      if go_next
+        nextInput e.target
+        go_next = no
+
+      if go_prev
+        prevInput e.target
+        go_prev = no
+
+      ip = ""
+      els.each (idx, el) ->
+        ip += el.value + (if idx is 3 then "" else ".")
+
+      this.val (if ip is "..." then "" else ip)
+
+      opts.callback.call(this) if opts.callback
+
+      return e
+
+  this
+) jQuery
---
+++ jquery.ipmask.js
@@ -0,0 +1,129 @@
+(function() {
+
+  (function($) {
+    var codesToNumbers, go_next, go_prev, isKey, keyCodes, nextInput, prevInput;
+    keyCodes = {
+      IGNORE: [65, 66, 67, 68, 69, 70, 71, 72, 73, 74, 75, 76, 77, 78, 79, 80, 81, 82, 83, 84, 85, 86, 87, 88, 89, 90, 106, 107, 109, 186, 187, 188, 189, 191, 192, 219, 220, 221, 222],
+      NUMBERS: [48, 49, 50, 51, 52, 53, 54, 55, 56, 57, 96, 97, 98, 99, 100, 101, 102, 103, 104, 105],
+      BACKSPACE: [8],
+      POINT: [190, 110]
+    };
+    codesToNumbers = {
+      48: 0,
+      49: 1,
+      50: 2,
+      51: 3,
+      52: 4,
+      53: 5,
+      54: 6,
+      55: 7,
+      56: 8,
+      57: 9,
+      96: 0,
+      97: 1,
+      98: 2,
+      99: 3,
+      100: 4,
+      101: 5,
+      102: 6,
+      103: 7,
+      104: 8,
+      105: 9
+    };
+    go_next = false;
+    go_prev = false;
+    nextInput = function(el) {
+      var next;
+      next = $(el).next().next();
+      if (next.length > 0) {
+        next.focus();
+      }
+    };
+    prevInput = function(el) {
+      var prev;
+      prev = $(el).prev().prev();
+      if (prev.length > 0) {
+        prev.focus();
+      }
+    };
+    isKey = function(arr, keycode) {
+      return arr.indexOf(keycode) !== -1;
+    };
+    $.fn.ip = function(opts) {
+      var container, els, html, i, _i, _ref,
+        _this = this;
+      if (opts == null) {
+        opts = {
+          placeholder: "...",
+          callback: null
+        };
+      }
+      opts.placeholder = (_ref = opts.placeholder) != null ? _ref.split(".") : void 0;
+      this.attr("type", "hidden");
+      container = document.createElement("div");
+      container.className = "b-ipmask form-control";
+      html = "";
+      for (i = _i = 0; _i <= 3; i = ++_i) {
+        html += "<input type='text' class='b-ipmask__input' placeholder='" + opts.placeholder[i] + "' maxlength='3'>";
+        if (i < 3) {
+          html += "<span class='b-ipmask__span'>.</span>";
+        }
+      }
+      container.innerHTML = html;
+      this.after(container);
+      els = $(".b-ipmask__input", container);
+      els.on("keydown", function(e) {
+        var sum, value;
+        value = this.value;
+        if (isKey(keyCodes.IGNORE, e.keyCode)) {
+          e.preventDefault();
+        } else if (isKey(keyCodes.NUMBERS, e.keyCode)) {
+          if (value.length === 2) {
+            sum = value + codesToNumbers[e.keyCode];
+            if (sum.length !== (parseInt(sum) + "").length || sum > 255) {
+              e.preventDefault();
+            } else {
+              go_next = true;
+            }
+          }
+        } else if (isKey(keyCodes.POINT, e.keyCode)) {
+          e.preventDefault();
+          if (value !== "") {
+            nextInput(this);
+          }
+        } else if (isKey(keyCodes.BACKSPACE, e.keyCode)) {
+          if (value.length === 0) {
+            prevInput(this);
+          } else {
+            if (value.length === 1) {
+              go_prev = true;
+            }
+          }
+        }
+        return e;
+      });
+      return els.on("keyup", function(e) {
+        var ip;
+        if (go_next) {
+          nextInput(e.target);
+          go_next = false;
+        }
+        if (go_prev) {
+          prevInput(e.target);
+          go_prev = false;
+        }
+        ip = "";
+        els.each(function(idx, el) {
+          return ip += el.value + (idx === 3 ? "" : ".");
+        });
+        _this.val((ip === "..." ? "" : ip));
+        if (opts.callback) {
+          opts.callback.call(_this);
+        }
+        return e;
+      });
+    };
+    return this;
+  })(jQuery);
+
+}).call(this);
